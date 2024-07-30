ed30bf9e9aa9fa79144b251c003fe0094daf3180
Matheus Levy <srlevy008@gmail.com>
2024-07-30 00:48:04 -0300

    movimentacao

--- Espaço Maluco/Assets/Scripts/PlayerController.cs
+++ Espaço Maluco/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 5;
+    [SerializeField] private Vector2 minhaPosicao;
+    
     // Start is called before the first frame update
     void Start()
     {
+        minhaPosicao = transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movimentacao();
+    }
+    //metodo para movimentacao do nosso player
+    void Movimentacao()
+    {
+
+     
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            minhaPosicao.y = minhaPosicao.y + Time.deltaTime * speed;
+            transform.position = minhaPosicao;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            minhaPosicao.y = minhaPosicao.y + Time.deltaTime * -speed;
+            transform.position = minhaPosicao;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            minhaPosicao.x = minhaPosicao.x + Time.deltaTime * -speed;
+            transform.position = minhaPosicao;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            minhaPosicao.x = minhaPosicao.x + Time.deltaTime * speed;
+            transform.position = minhaPosicao;
+        }
+        
+        
         
     }
 }
