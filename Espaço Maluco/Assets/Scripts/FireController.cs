ecd5ece1a78ea76f595bb58dbfd48b92b56dc309
Matheus Levy <srlevy008@gmail.com>
2024-08-01 12:30:41 -0300

    cooking fire :)

--- Espaço Maluco/Assets/Scripts/FireController.cs
+++ Espaço Maluco/Assets/Scripts/FireController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class FireController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    private Rigidbody2D myRb;
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRb.velocity = Vector2.up*speed;
     }
 }
